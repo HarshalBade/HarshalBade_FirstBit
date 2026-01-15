cf26ce17bde9dfcf32f17fb3df2d88e1caa01be2
Harshal Bade <128248096+HarshalBade@users.noreply.github.com>
2026-01-15 23:34:01 +0530

    Update BasedSalary.c

--- C Module/Assignments/Assignment 1/BasedSalary.c
+++ C Module/Assignments/Assignment 1/BasedSalary.c
@@ -1,1 +1,17 @@
-
+#include<stdio.h>
+void main(){
+	int basic=10000;
+	int da, ta, hra;
+	if(basic<=5000){
+		da = basic * 0.10;
+		ta = basic * 0.20;
+		hra = basic * 0.25;
+	}else{
+		da = basic * 0.15;
+		ta = basic * 0.25;
+		hra = basic * 0.30;
+	}
+	printf("da: %d\nta: %d\nhra: %d", da,ta,hra);
+	int total = basic+da+ta+hra;
+	printf("\nTotal salary : %d", total);
+}
