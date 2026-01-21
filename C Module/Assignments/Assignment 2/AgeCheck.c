4c3a18bd8d4d8cc5c832d4c61bfbdbec03f4fa82
Harshal Bade <128248096+HarshalBade@users.noreply.github.com>
2026-01-21 23:13:32 +0530

    Update AgeCheck.c

--- C Module/Assignments/Assignment 2/AgeCheck.c
+++ C Module/Assignments/Assignment 2/AgeCheck.c
@@ -1,1 +1,18 @@
-
+#include<stdio.h>
+void main(){
+	int age;
+	printf("Enter the Age: ");
+	scanf("%d",&age);
+	
+	if(age>=0 && age<12){
+		printf("You are Child");
+	}else if(age>=12 && age<=19){
+		printf("You are Teenager");
+	}else if(age>=20 && age<=59){
+		printf("You are Adult");
+	}else if(age>=60){
+		printf("You are Senior");
+	}else{
+		printf("Age is incorrect");
+	}
+}
