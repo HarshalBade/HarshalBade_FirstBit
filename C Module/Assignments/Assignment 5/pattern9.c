3bcd1c5dde7a723be482b491ffccf65c74bb888b
Harshal Bade <128248096+HarshalBade@users.noreply.github.com>
2026-02-04 18:38:06 +0530

    Add files via upload

---
+++ C Module/Assignments/Assignment 5/pattern9.c
@@ -0,0 +1,14 @@
+/*  1 
+	2 3 
+	4 5 6 
+	7 8 9 10 */
+	
+#include<stdio.h>
+void main(){
+	int n=4,ct=1;
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=i;j++)
+			printf("%d ",ct++);
+		printf("\n");
+	}
+}
