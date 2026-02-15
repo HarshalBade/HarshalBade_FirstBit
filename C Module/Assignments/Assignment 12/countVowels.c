e6be0617347983e296867a952cf1835132dffbc8
Harshal Bade <128248096+HarshalBade@users.noreply.github.com>
2026-02-15 19:23:42 +0530

    Update countVowels.c

--- C Module/Assignments/Assignment 12/countVowels.c
+++ C Module/Assignments/Assignment 12/countVowels.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+#include<string.h>
+void main(){
+	char str[20];
+	int count=0;
+	printf("Enter string: ");
+    scanf("%s", str);
+    
+    for(int i=0;str[i]!='\0';i++)
+    	if(strchr("aeiouAEIOU",str[i])!=NULL)
+    		count++;
+	
+    printf("Vowels count: %d", count);
+}
