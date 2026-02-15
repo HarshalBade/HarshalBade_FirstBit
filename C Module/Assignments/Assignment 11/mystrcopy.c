28f002cadf0ef1703ff6e3a1c5db52a472eff7ad
Harshal Bade <128248096+HarshalBade@users.noreply.github.com>
2026-02-15 19:11:30 +0530

    Update mystrcopy.c

--- C Module/Assignments/Assignment 11/mystrcopy.c
+++ C Module/Assignments/Assignment 11/mystrcopy.c
@@ -1,1 +1,17 @@
-
+#include<stdio.h>
+char* mystrcopy(char*,char*);
+void main(){
+	char str[10]="harshal";
+	char str2[10];
+	
+	mystrcopy(str2,str);
+	printf("Copy String: %s",str2);
+}
+char* mystrcopy(char* str2,char* str){
+	int i=0;
+	while(str[i]!='\0'){
+		str2[i]=str[i];
+		i++;
+	}
+	str2[i]='\0';
+}
